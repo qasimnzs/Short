95307a3a10442ccfaac3990cf584789e61460a48
qasimnzs <85975321+qasimnzs@users.noreply.github.com>
2026-04-05 22:36:37 +0500

    Update page.js

--- app/[shortId]/page.js
+++ app/[shortId]/page.js
@@ -1,15 +1,19 @@
 import { Redis } from '@upstash/redis';
 import { redirect } from 'next/navigation';
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
-})
+// Ye line build error ko fix karegi
+export const dynamic = 'force-dynamic';
 
-export default async function RedirectPage({ params }: any) {
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function Page({ params }) {
   const { shortId } = params;
   
-  const longUrl: string | null = await redis.get(shortId);
+  // Link check karein
+  const longUrl = await redis.get(shortId);
 
   if (!longUrl) {
     redirect('/');
