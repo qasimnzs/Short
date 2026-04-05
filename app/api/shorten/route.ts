93c7ff3cc84eb8e99444a031aa8b1e5cc0163631
qasimnzs <85975321+qasimnzs@users.noreply.github.com>
2026-04-05 10:18:15 +0500

    Create route.ts

---
+++ app/api/shorten/route.ts
@@ -0,0 +1,22 @@
+import { Redis } from '@upstash/redis';
+import { nanoid } from 'nanoid';
+import { NextRequest, NextResponse } from 'next/server';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+})
+
+export async function POST(req: NextRequest) {
+  try {
+    const { url } = await req.json();
+    if (!url) return NextResponse.json({ error: 'URL is required' }, { status: 400 });
+
+    const shortId = nanoid(6);
+    await redis.set(shortId, url);
+
+    return NextResponse.json({ shortId });
+  } catch (error) {
+    return NextResponse.json({ error: 'Server Error' }, { status: 500 });
+  }
+}
