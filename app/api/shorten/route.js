b1e8bfc7498b5353201caa7e6d2c11d235878b5c
qasimnzs <85975321+qasimnzs@users.noreply.github.com>
2026-04-05 22:56:37 +0500

    Update route.js

--- app/api/shorten/route.js
+++ app/api/shorten/route.js
@@ -3,7 +3,6 @@ import { nanoid } from 'nanoid';
 import { NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
-
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN,
@@ -11,11 +10,20 @@ const redis = new Redis({
 
 export async function POST(req) {
   try {
-    const { url } = await req.json();
+    const { url, title, desc, img } = await req.json();
     if (!url) return NextResponse.json({ error: 'URL is required' }, { status: 400 });
 
     const shortId = nanoid(6);
-    await redis.set(shortId, url);
+    
+    // Sab data ko ek object mein save kar rahe hain
+    const linkData = {
+      longUrl: url,
+      title: title || "Shared Link",
+      description: desc || "Click to open the link",
+      image: img || "https://vercel.com/og-image.png"
+    };
+
+    await redis.set(shortId, JSON.stringify(linkData));
 
     return NextResponse.json({ shortId });
   } catch (error) {
